bf8d67868afa4e00667d3135cd603496d1cd7723
purge710 <johnrussel338@gmail.com>
2023-11-29 00:52:35 +0800

    bunal!

--- src/App.js
+++ src/App.js
@@ -1,66 +1,67 @@
-import React from 'react';
-import { ReactDOM } from 'react';
+import React, { useState } from 'react';
 import './App.css';
-//hook para makuha ang data na gikan sa users na naka object sulod sa FakeData.js
 import { useSelector, useDispatch } from 'react-redux';
-//ad ug bag-ong user / updateUser / deleteUser
 import { addUser, updateUsername, deleteUser } from './features/Users';
-//add useStateHook
-import { useState } from 'react';
-
-
 
 function App() {
-  //kuhaon ang value sa name ug username gamit onchange!
   const [name, setName] = useState("");
   const [username, setUsername] = useState("");
-
-  //for update
   const [newUsername, setNewUsername] = useState("");
 
   const dispatch = useDispatch();
+  const userList = useSelector((state) => state.users.value);
 
+  const getNextUserId = () => {
+    if (userList.length === 0) {
+      return 1; // If userList is empty, start from ID 1
+    }
+    return userList[userList.length - 1].id + 1;
+  };
 
-  //kuhaon ang data
-  const userList = useSelector((state) => state.users.value)
-
+  const handleAddUser = () => {
+    const newUser = {
+      id: getNextUserId(),
+      name: name,
+      username: username,
+    };
+    dispatch(addUser(newUser));
+  };
 
   return (
     <div className="App">
-    <h1>RDX</h1>
+      <h1>RDX</h1>
       <div className='addUser'>
-        <input type="text" 
-          onChange={(x) => {setUsername(x.target.value)}}
-          placeholder='Name...' />
-        <input type="text" 
-          onChange={(e) => {setUsername(e.target.value)}} 
-          placeholder='Username...' />
-        <button onClick={() => { dispatch(addUser({ id: userList[userList.length - 1].id + 1, name, username })) }}>Add User</button>
+        <input
+          type="text"
+          onChange={(e) => setName(e.target.value)}
+          placeholder='Name...'
+        />
+        <input
+          type="text"
+          onChange={(e) => setUsername(e.target.value)}
+          placeholder='Username...'
+        />
+        <button onClick={handleAddUser}>Add User</button>
       </div>
 
       <div className='displayUsers'>
-        {userList.map((user) => {
-          return (
-            <div>
-              <h1>{user.name}</h1>
-              <h1>{user.username}</h1>
-              <input type="text" 
-                onChange={(w) => {setNewUsername(w.target.value)}} 
-                placeholder='New username' />
-                <button 
-                onClick={() => { dispatch(updateUsername({ id: user.id, username: newUsername})
-                ); 
-              }}
-              >
+        {userList.map((user) => (
+          <div key={user.id}>
+            <h1>{user.name}</h1>
+            <h1>{user.username}</h1>
+            <input
+              type="text"
+              onChange={(e) => setNewUsername(e.target.value)}
+              placeholder='New username'
+            />
+            <button onClick={() => dispatch(updateUsername({ id: user.id, username: newUsername }))}>
               Update Username
-              </button>
-                <button
-                onClick={() => { dispatch(deleteUser({ id: user.id})) }}
-                >Delete User</button>
-            </div>
-        
-          )
-        })}
+            </button>
+            <button onClick={() => dispatch(deleteUser({ id: user.id }))}>
+              Delete User
+            </button>
+          </div>
+        ))}
       </div>
     </div>
   );
